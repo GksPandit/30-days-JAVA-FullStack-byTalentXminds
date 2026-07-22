7de6dc357b6760d9009375142a093311317f0dd4
GksPandit <jikeshkumar841439@gmail.com>
2026-07-22 16:46:43 +0530

    Completed Day 8 OOP concepts

--- src/Day8/oops.java
+++ src/Day8/oops.java
@@ -1,15 +1,52 @@
-package Day8;
 
+package Day8;
 public class oops {
+
     public static void main(String[] args) {
 
+        // Arrays
+        int[] rollNo = new int[5];
+        String[] names = new String[5];
+        float[] marks = new float[5];
 
-        //  student -it will be stored inside our stack memory at the compile time
-        // objects will be stored inside heap memory runtime
-        oops student1 = new oops();
-       oops student2 = new oops();
+        // Objects
+        Student student1 = new Student(1, "Nikhil", 87.6);
+        Student student2 = new Student(2, "Rahul", 99);
 
+        // Update marks
+        student1.marks = 90.5f;
 
+        // Print objects
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
 
+// Blueprint
+class Student {
+
+    // Properties
+    int rollNo;
+    String name;
+    double marks;
+
+    // Constructor
+    public Student(int rollNo, String name, double marks) {
+
+        System.out.println("Constructor Called");
+
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
     }
 }
